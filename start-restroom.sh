6b44bdb132c6c066bda6aa9493b39a77e323251b
Alberto Lerda <albertolerda97@gmail.com>
2022-12-09 20:21:21 +0100

    fix

--- start-restroom.sh
+++ start-restroom.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 RESTROOM_CONTAINER_NAME=$1
+CONTRACTS="$2"
 
 echo "Starting restroom instance"
-docker run --name $RESTROOM_CONTAINER_NAME --publish 3000:3000 -v "/home/runner/work/restroom-github-action/restroom-github-action/contracts":"/app/contracts" --detach ghcr.io/albertolerda/restroom-github-action:main
+echo "Using contracts in $CONTRACTS"
+docker run --name $RESTROOM_CONTAINER_NAME --publish 3000:3000 -v "$CONTRACTS":"/app/contracts" --detach ghcr.io/albertolerda/restroom-github-action:main
 
